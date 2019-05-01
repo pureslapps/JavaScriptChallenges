035bbf1f6c008c42dbc5973f5fdb037eb02d59ac
Jorge Bhagwandeen <jorgebhagwandeen@yahoo.com>
2019-05-01 13:01:47 -0400

    added more work after cloning

--- main.js
+++ main.js
@@ -5,3 +5,13 @@ function getSum(total, num) {
   return total + num;
 }
 console.log("The sum is:", sum);
+
+
+for (let i = 99; i >= 1; i--) {
+  if (i !== 1) { //if ((i-1) === 1)
+    console.log(i + ' crows on the wall. ' + i + ' crows. ' + ' 1 fell down and become a wight. ' + (i - 1) + ' crows on the wall. ');
+  }
+  else {
+    console.log ('1 single crow. It fell down and became a wignt. There is no one left to defend Westeros now. ');
+  }
+}
